9dff740b7be40053c938d0994499cd004b60aee1
mataProDigital <mataprousa@gmail.com>
2024-02-16 11:46:11 -0800

    creando menu de opciones(crud)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # mi_agenda_py
+
--- agenda.py
+++ agenda.py
@@ -1,20 +1,35 @@
-# Importamos el módulo 'os' que nos permite interactuar con el sistema operativo
+# Importas el módulo 'os', que proporciona funciones para interactuar con el sistema operativo.
 import os
 
+# Definiste una constante 'CARPETA' que contiene el nombre del directorio que quieres crear.
 CARPETA = 'contactos/'
 
-# Definimos la función principal de nuestra aplicación
+# Definiste la función principal de tu aplicación, llamada 'app'.
 def app():
-    # Llamamos a la función 'crear_directorio'
+    # Dentro de esta función, llamas a otra función llamada 'crear_directorio'.
     crear_directorio()
 
-# Definimos la función 'crear_directorio'
+    # Muestra el menu de opciones
+    mostrar_menu()
+    
+def mostrar_menu():
+    print('Seleccione del menu lo que desea hacer:')
+    print('1) Agregar nuevo contacto')
+    print('2) Editar contacto')
+    print('3) Ver contactos')
+    print('4) Buscar contacto')
+    print('5) Eliminar contacto')
+    
+
+# Aquí defines la función 'crear_directorio'.
 def crear_directorio():
-     # Usas 'os.path.exists' para verificar si el directorio 'contactos/' ya existe.
+    # Usas 'os.path.exists' para verificar si el directorio especificado por 'CARPETA' ya existe.
     if not os.path.exists(CARPETA):
-     # Si el directorio no existe, usas 'os.makedirs' para crearlo.
+        # Si el directorio no existe, usas 'os.makedirs' para crearlo.
         os.makedirs(CARPETA)
     else:
-        print('La  carpeta ya existe')
+        # Si el directorio ya existe, imprimes un mensaje informando al usuario.
+        print('La carpeta ya existe')
+
 # Finalmente, llamas a la función principal 'app' para iniciar tu aplicación.
 app()
