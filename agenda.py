8ee7495ccda9e27d6d68e125a35cc87154539ab5
mataProDigital <mataprousa@gmail.com>
2024-02-16 11:58:40 -0800

    creando condicional para cada opcion

--- agenda.py
+++ agenda.py
@@ -11,7 +11,33 @@ def app():
 
     # Muestra el menu de opciones
     mostrar_menu()
-    
+
+    #preguntar al usuario la accion a realizar
+
+    preguntar =True
+    while preguntar:
+        opcion = input('Seleccione una opcion: \r\n')
+        opcion = int(opcion)
+
+        #ejecutar las opciones
+        if opcion == 1:
+            print('Agregar contacto')
+            preguntar = False
+        elif opcion == 2:
+            print('Editar contacto')
+            preguntar = False
+        elif opcion == 3:
+            print('Ver contactos')
+            preguntar = False
+        elif opcion == 4:
+            print('Buscar contacto')
+            preguntar = False
+        elif opcion == 5:
+            print('Eliminar contacto')
+            preguntar = False
+        else:
+            print('Opcion no valida, intente de nuevo')
+
 def mostrar_menu():
     print('Seleccione del menu lo que desea hacer:')
     print('1) Agregar nuevo contacto')
